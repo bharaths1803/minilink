11b15c826a481f37c6853c1aaf0800ed3fcd06bf
bharaths1803 <bharaths14051803@gmail.com>
2025-06-23 20:58:27 +0530

    Changed loading.tsx

--- app/(main)/link/[linkId]/loading.tsx
+++ app/(main)/link/[linkId]/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
     <div className="flex h-[80vh] items-center justify-center">
-      <div className="h-10 w-10 border-4 border-white  rounded-full animate-spin" />
+      <div className="h-10 w-10 border-4 border-white border-t-transparent  rounded-full animate-spin" />
     </div>
   );
 }
